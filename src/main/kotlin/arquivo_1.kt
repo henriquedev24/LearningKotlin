d9dab02a228202ab3ba814ee60910d1cff2d496b
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-08-09 17:26:11 -0300

    aprendendo sobre kotlin

---
+++ src/main/kotlin/arquivo_1.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val name = "gabriel henrique"
+    val age = "20"
+
+    println(name + " " + age)
+}
---
+++ src/main/kotlin/tipos-variaveis.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var x: Int = 20
+    val y: Int
+    var a: Float = 19.50f
+    var b: Double = 2.14481344
+    var c: Long = 10100011011010110
+    var d: Char = 'H'
+    var e: String = "Gabriel"
+    var f: Short = 200
+    var g: Byte = 0b1001
+    var h: Byte = 0xa
+    var i: UInt = 15u
+
+    println(x)
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+}
