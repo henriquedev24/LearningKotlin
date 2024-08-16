bf7647f74ee6079519d23cc91585388409bf2819
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-08-16 20:10:15 -0300

    learn many things

--- src/main/kotlin/leitura-de-dados-java.kt
+++ src/main/kotlin/leitura-de-dados-java.kt
@@ -1,7 +1,45 @@
-fun ler.int(){
+import java.util.Scanner
 
+fun ler_int(){
+    var scanner = Scanner(System. `in`)
+    println("Digite número inteiro: ")
+    val num = scanner.nextInt()
+    println("Você digitou: ${num}")
+}
+
+fun ler_double(){
+    val scanner = Scanner(System. `in`)
+    println("Digite número double: ")
+    val num = scanner.nextDouble()
+    println("Você digitou: ${num}")
+}
+
+fun ler_string(){
+    val scanner = Scanner(System. `in`)
+    println("Digite nome: ")
+    val num = scanner.next()
+    println("Você digitou: ${num}")
+}
+
+fun ler_float(){
+    val scanner = Scanner(System. `in`)
+    println("Digite número float: ")
+    val num = scanner.nextFloat()
+    println("Você digitou: ${num}")
 }
 
 fun main(){
-     
+    ler_int()
+    ler_double()
+    ler_string()
+    ler_float()
+    ler_boolean()
+}
+
+fun ler_boolean(){
+    val scanner = Scanner(System. `in`)
+    println("Digite valor boolean: ")
+    val num = scanner.nextBoolean()
+    println("Você digitou: ${num}")
+    scanner.close()
 }
